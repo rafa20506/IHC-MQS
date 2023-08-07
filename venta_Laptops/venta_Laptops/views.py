2768156114339bf77d717c3b3872a842fc09965c
Miguel Bernal <mb123mb1@hotmail.com>
2023-08-07 18:53:21 -0400

    funciona el html

--- venta_Laptops/venta_Laptops/views.py
+++ venta_Laptops/venta_Laptops/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.template import Template, Context
 
 def saludo(request):
-    documento = open("../venta_Laptops/venta_Laptops/vistas/index.html")   # la direccion relativa de los archivos html, css "vistas"
+    documento = open("../venta_Laptops/venta_Laptops/vistas/index2.html")   # la direccion relativa de los archivos html, css "vistas"
     
     plt = Template(documento.read())  #copiar siempre
     documento.close()               #copiar siempre
@@ -24,7 +24,7 @@ def despedida(request, nombre):   # lo mismo de arriba pero con parametros
     plt = Template(documento.read())  #copiar siempre
     documento.close()               #copiar siempre
 
-    ctx = Context()                 #copiar siempre
-    documentoMostrar = plt.render(ctx)%(nombre) #   %("joaquin perrra")  #copiar siempre y aqui agregar las variables todo esto
+    ctx = Context({"nombre":nombre})     # aqui se modifica el apso de parametros   (tambien modificar el html)         #copiar siempre
+    documentoMostrar = plt.render(ctx) #     #copiar siempre 
 
     return HttpResponse(documentoMostrar)  # el elemento que mostrara la paguina o web
